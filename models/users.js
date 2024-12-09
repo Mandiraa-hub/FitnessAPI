ce7ab42b736128b4f4503eefb8816cfd3985c5f2
Mandiraa-hub <mairakhanal2060@gmail.com>
2024-12-09 19:03:45 +0545

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
